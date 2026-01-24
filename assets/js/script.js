f96ec5157e179f47c8e01737f24f4189b6839418
PRAVEEN GUTTULA <praveenguttula4@gmail.com>
2026-01-24 14:35:33 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,11 +59,11 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Portfolio | CodeWithTanveer";
+            document.title = "Portfolio | CodeWithPraveen";
             $("#favicon").attr("href", "assets/images/favicon.jpg");
         }
         else {
-            document.title = "Portfolio | Tanveer Ahmad";
+            document.title = "Portfolio | Praveen";
             $("#favicon").attr("href", "assets/images/favicon.jpg");
         }
     });
